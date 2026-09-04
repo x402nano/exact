75f3ac5264d28b4061200eb3efcfeb79c3ee3250
Frank Kilkelly <contact@nanocharts.info>
2026-09-04 14:42:11 +0100

    Remove unused validation function

--- src/typescript/common.ts
+++ src/typescript/common.ts
@@ -1,7 +1,1 @@
-import type { ZodTypeAny } from 'zod'
-
-export function validate(zodSchema: ZodTypeAny, toParse: any): boolean {
-  return zodSchema.safeParse(toParse).success
-}
-
 export const CURRENCY_CODE_XNO = 'XNO'
